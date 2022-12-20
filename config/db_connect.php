ed94e80439cfc18b4da21fa257ba4bb82c228738
dac <huyng97@yahoo.com>
2022-12-20 17:06:25 -0600

    heroku

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,10 +1,14 @@
 <?php
-function pdo_connect_mysql2()
+function pdo_connect_mysql()
 {
-	$DATABASE_HOST = 'localhost';
-	$DATABASE_USER = 'root';
-	$DATABASE_PASS = '';
-	$DATABASE_NAME = 'brimstone';
+	// $DATABASE_HOST = 'localhost';
+	// $DATABASE_USER = 'root';
+	// $DATABASE_PASS = '';
+	// $DATABASE_NAME = 'brimstone';
+	$DATABASE_HOST = 'us-cdbr-east-06.cleardb.net';
+	$DATABASE_USER = 'b8cc149d67668a';
+	$DATABASE_PASS = '67f2ea24';
+	$DATABASE_NAME = 'heroku_6fa303fc5bb9939';
 	try {
 		return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
 	} catch (PDOException $exception) {
@@ -13,18 +17,16 @@ function pdo_connect_mysql2()
 	}
 }
 
-function pdo_connect_mysql()
-{
-	//Get Heroku ClearDB connection information
-	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-  $cleardb_server = $cleardb_url["host"];
-  $cleardb_username = $cleardb_url["user"];
-  $cleardb_password = $cleardb_url["pass"];
-  $cleardb_db = substr($cleardb_url["path"],1);
-  $active_group = 'default';
-  $query_builder = TRUE;
-  // Connect to DB
-  $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-	return $conn;
-}
+// function pdo_connect_mysql2()
+// {
+// 	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//   $cleardb_server = $cleardb_url["host"];
+//   $cleardb_username = $cleardb_url["user"];
+//   $cleardb_password = $cleardb_url["pass"];
+//   $cleardb_db = substr($cleardb_url["path"],1);
+//   $active_group = 'default';
+//   $query_builder = TRUE;
+//   $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// 	return $conn;
+// }
 ?>
